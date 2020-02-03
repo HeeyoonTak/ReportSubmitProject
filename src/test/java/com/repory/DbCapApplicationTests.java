f93f8055462f17efafe8b47c38a6e6cf891a2eab
'Heeyoon <'wewe1003@naver.com'>
2020-02-03 16:33:11 +0900

    초기설정

---
+++ src/test/java/com/repory/DbCapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
